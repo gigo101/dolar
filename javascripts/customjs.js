0744032d981fd71c01e59a2c4e6bf0a792980701
gigo101 <ianjay.padios@gmail.com>
2019-01-27 22:20:33 +0800

    Update customjs

--- javascripts/customjs.js
+++ javascripts/customjs.js
@@ -1,24 +1,63 @@
-$('document').ready(function(){
-      /*
-      $("#save").click(function(){
-            $("#save").attr("data-dismiss","modal");
-            $('#exampleModal').modal('hide');
-            $('#testModal').modal('show'); 
-      });
+/*
+$("#save").click(function(){
+    $("#save").attr("data-dismiss","modal");
+        $('#exampleModal').modal('hide');
+        $('#testModal').modal('show'); 
+});
       
-      */
+*/
 
-      $('#registertxt').click(function(e){
-          e.preventDefault();
+$('#registertxt').click(function(e){
+    e.preventDefault(); 
 
-          $('#loginModal')
-              .modal('hide')
-              .on('hidden.bs.modal', function (e) {
+        $('#loginModal')
+            .modal('hide')
+            .on('hidden.bs.modal', function (e) {
                   $('#registerModal').modal('show');
 
                   $(this).off('hidden.bs.modal'); // Remove the 'on' event binding
-              });
+        });
 
-      });
+});
 
+ /*
+      $('form').on('submit', function (e) {
+
+          e.preventDefault();
+
+          $.ajax({
+            type: 'post',
+            url: 'index.php',
+            data: $('form').serialize(),
+            success: function () {
+              alert('form was submitted');
+            }
+          });
+
+        });
+    
+  */
+
+$('#registerCustomer').on('submit',function(e) {
+  $.ajax({
+          type: "post",
+          url: "insert_customer.php",
+          data: $('#registerCustomer').serialize(),
+
+         success: function(){
+              alert('Data Save');
+         }
     });
+ });
+ 
+
+/* display message after form is submitted
+
+$("form").on('submit',function(e){
+        alert("Form submitted");
+
+});
+
+
+*/
+
