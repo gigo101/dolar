f2885ffef1ee6f1f4be740cad06215bf35abfc3f
gigo101 <ianjay.padios@gmail.com>
2019-01-27 22:20:02 +0800

    update insert customer

--- insert_customer.php
+++ insert_customer.php
@@ -1,3 +1,9 @@
+<?php
+
+	require_once('includes/dbconnection.php');
+
+?>
+
 
 <?php
 //Get the data from the subject.php form using post method
@@ -8,33 +14,17 @@ $address=$_POST["address"];
 ?>
 
 <?php
-//1. Create a database connection
-$dbhost="localhost";
-$dbuser="root";
-$dbpass="";
-$dbname="dolardb";
-$connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-
-//test if the connection occured
-	if(mysqli_connect_errno()){
-		die("Database connection failed: " . mysqli_connect_errno() . "(".mysqli_connect_errno() .")"
-	);
-}
-
-?>
-
-
-<?php
 
 if(isset($_REQUEST)){
 
 
 //2. Perform database query
-	$query="INSERT INTO tblcustomers (username,password,contactNumber,address) VALUES ('{$username}','{$password}','{$contactNumber}','{$address}')";
+	$query="INSERT INTO tblcustomers (username,password,contactNumber,address) VALUES ('{$username}','{$password}','{$contactNumber}','{$address}')";	
 	$result=mysqli_query($connection,$query);
 	if($result){
 		//Data insertion successful
-		header("Location: databases.php");
+		//header("Location: databases.php");
+		alert("Data successfully save.");
 	}else
 	{	
 		//insertion is not sucessful
@@ -49,8 +39,3 @@ if(isset($_REQUEST)){
 	//4. Release returned data
 		mysqli_free_result($result);
 	?>
-
-<?php
-	//5. Close database connection
-	mysqli_close($connection);
-?>
