7369f35e8d0a17118328b4f0e2009c05adc43def
gigo101 <ianjay.padios@gmail.com>
2019-01-27 19:42:33 +0800

    update index

--- index.php
+++ index.php
@@ -1,75 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Dolar Outsourcing</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-
-
-    <!-- Bootstrap core CSS     -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Animation library for notifications   -->
-    <link href="assets/css/animate.min.css" rel="stylesheet"/>
-
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
-
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
-</head>
-<body>
-
-<div class="wrapper">
-    <div>
-        <nav class="navbar navbar-default navbar-fixed">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Dollar Outsourcing</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                           <li>
-                           <a href="">
-                               Register
-                            </a>
-                        </li>
-                          <li>
-                           <a href="">
-                               Login
-                            </a>
-                        </li>
-                         <li>
-                           <a href="">
-                               Services
-                            </a>
-                        </li>
-                        <li>
-                           <a href="">
-                               Contact
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+<!--Header-->
+<?php
+    
+    include 'includes/header.php';
+?>
 <main role="main" style="" class="main1">
 
   <!-- Main jumbotron for a primary marketing message or call to action -->
@@ -211,92 +145,10 @@
 
 </div> <!-- /container -->
 
-</main>
+<!--Footer-->
 
-        <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-
-                    </ul>
-                </nav>
-                <p class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script> Dolar Outsourcing
-                </p>
-            </div>
-        </footer>
-
-    </div>
-</div>
-
-
-</body>
-
-  <!--   Core JS Files   -->
-  <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
-	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-
-	<!--  Charts Plugin -->
-	<script src="assets/js/chartist.min.js"></script>
-
-  <!--  Notifications Plugin    -->
-  <script src="assets/js/bootstrap-notify.js"></script>
-
-  <!--  Google Maps Plugin    -->
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-
-  <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-	<script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
-
-	<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
-	<script src="assets/js/demo.js"></script>
-
-  <!-- Custom JS Scripts-->
-  <script src="javascripts/customjs.js"></script>
-
-  <script type="text/javascript">
-    /*
-      $('form').on('submit', function (e) {
-
-          e.preventDefault();
-
-          $.ajax({
-            type: 'post',
-            url: 'index.php',
-            data: $('form').serialize(),
-            success: function () {
-              alert('form was submitted');
-            }
-          });
-
-        });
-    
-  */
+<?php
   
-  $('#registerCustomer').on('submit',function(e) {
-  $.ajax({
-          type: "post",
-          url: "insert_customer.php",
-          data: $('#registerCustomer').serialize(),
+    include 'includes/footer.php';
 
-         success: function(){
-              alert('Data Save');
-         }
-});
- });
- 
-
-$("form").on('submit',function(e){
-        alert("Form submitted");
-
-    });
-  </script>
-<!--
-  PADI (~_~)
-!-->
-</html>
+?>
