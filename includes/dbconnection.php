13720a81bdb97244cecb6be9e05a575818cafbf4
gigo101 <ianjay.padios@gmail.com>
2019-02-05 19:51:13 +0800

    updated dbonnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,12 +1,11 @@
 <?php
 
-define(DB_HOST, "localhost");
-define(DB_USER, "root");
-define(DB_PASSWORD, "");
-define(DB_NAME, "dolardb");
-//1. Create a database connection
 $dbhost="localhost";
-$connection=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$dbuser="root";
+$dbpassword="";
+$dbname="dolardb";
+//1. Create a database connection
+$connection=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 
 //test if the connection occured
 	if(mysqli_connect_errno()){
